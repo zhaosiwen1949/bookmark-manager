253f971b13d6f962ff169b5febde4eec9874fd8d
caoyasong <caoyasong1949@163.com>
2018-08-20 15:19:24 +0800

    开发环境基本搭好

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-import path from 'path';
+const path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    mode: 'development',
+    entry: path.resolve(__dirname, './src/index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -19,5 +20,6 @@ module.exports = {
             exclude: /node_modules/,
             use: 'babel-loader'
         }]
-    }
+    },
+    devtool: "eval-source-map"
 }
