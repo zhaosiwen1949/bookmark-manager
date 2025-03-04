dbba69bfdf9aeed3519ac589afdb64fd91027b39
yangdongmei <18848952893@163.com>
2025-03-04 17:25:05 +0800

    feat: 升级 manifest.json 的 manifest_version 为3

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,41 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, './src/index.jsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+  mode: "development",
+  entry: path.resolve(__dirname, "./src/index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".json"],
+    alias: {
+      react: "preact-compat",
+      "react-dom": "preact-compat",
     },
-    resolve: {
-        extensions: ['.jsx', '.js', '.json'],
-        alias: {
-            'react': 'preact-compat',
-            'react-dom': 'preact-compat'
-        }
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: 'babel-loader'
-        }]
-    },
-    devtool: "eval-source-map",
-    plugins: [
-        new CleanWebpackPlugin(['dist'],{
-            root: __dirname,
-        }),
-        new CopyWebpackPlugin([ 
-           {from: "./src/css/", to:"./css/"},
-           {from: "./src/images/", to:"./images/"},
-           {from: "./src/js/", to:"./js/"},
-           {from: "./src/manifest.json", to:"./manifest.json"},
-           {from: "./src/popup.html", to:"./popup.html"},
-        ])
-    ]
-}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  devtool: false,
+  plugins: [
+    new CleanWebpackPlugin(["dist"], {
+      root: __dirname,
+    }),
+    new CopyWebpackPlugin([
+      { from: "./src/css/", to: "./css/" },
+      { from: "./src/images/", to: "./images/" },
+      { from: "./src/js/", to: "./js/" },
+      { from: "./src/manifest.json", to: "./manifest.json" },
+      { from: "./src/popup.html", to: "./popup.html" },
+    ]),
+  ],
+};
